22b72e559ecedf8639c832475074688748fc2c79
Rodrigo Abadia <abadiarodrigo@gmail.com>
2018-10-11 10:31:13 +0200

    rastreador_archivos_receptor.sh_v1.0

---
+++ rastreador_archivos_receptor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Este programa se encargara de acceder al receptor con
+#el objetivo de comprobar si hay algun archivo que no se descargo.
+#Los Scripts check_empty_dir.sh y contains.sh deben estar en el mismo directorio que este Script.
+#A este programa hay que entregarle 2 argumentos
+#$1 El "Path" de donde estan los nombres de los archivos en la Raspberry pi.
+#$2 El "Path" de donde estan los nombres de los archivos en el Receptor.
+
+declare -i i
+
+rastreador()
+{
+    . check_empty_dir.sh                        #Me devolvera una d=0 si el directorio esta vacio y d=1 si no.
+    . contains.sh                               #Me devolvera una b=0 si el item se encuentra en la lista y b=1 si no.
+                                                
+    i=0
+
+    path_rasppi=$1                              #"Path" de donde estan los nombres de los archivos en la Raspberry pi.
+    path_receptor=$2                            #"Path" de donde estan los nombres de los archivos en el Receptor.
+                                                
+    for j in ${path_receptor[*]}; do            #El archivo datos contiene la lista de los archivos contenidos            
+        $j >> datos                             #en Dropbox o en el receptor.
+    done                                        #El archivo datos estara en el mismo directorio que este Script.
+
+    for j in $(ls $path_rasppi); do
+        contains $j 
+        check_empty "$path_rasppi/$j"
+        if [ "b"=="1" ]; then
+            wget -m ftp://everis:IFEGSA@192.168.3.1/DSK1/SSN/LOG1_everis_prueba_RINEX_24H/$j    #El "path" que se da en las lineas 29 y 31 es la del receptor.
+        elif [ "d"=="0"]; then
+            wget -m ftp://everis:IFEGSA@192.168.3.1/DSK1/SSN/LOG1_everis_prueba_RINEX_24H/$j
+        fi
+    done
+
+    rm datos
+}
